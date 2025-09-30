8503741d428881698d2f0fac087ea4cac990e706
sohaum <sohaumghosh@gmail.com>
2025-09-30 06:47:28 +0530

    [...nextauth]/route.ts

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,13 +1,14 @@
 // app/api/auth/[...nextauth]/route.ts
 import NextAuth from "next-auth";
 import { authOptions } from "@/lib/auth";
+import type { NextRequest } from "next/server";
 
 const handler = NextAuth(authOptions);
 
-export async function GET(req) {
+export async function GET(req: NextRequest) {
   return handler(req);
 }
 
-export async function POST(req) {
+export async function POST(req: NextRequest) {
   return handler(req);
 }
